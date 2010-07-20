9e86231689b551d38428dbf9666a7128877b5fc7
Zheng-Yu Chen <vvdpzz@gmail.com>
2010-07-20 10:54:19 +0800

    修改了CSS清零样式以及对ol,ul,li的支持

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
     return pi[0..n-1] if n-1>=0
   end
   def safe_html_out_for(object)
-    sanitize object, :tags => %w(h1 h2 h3 h4 h5 h6 p a img br span table th tr td b), :attributes => %w(id class style href src width height)
+    sanitize object, :tags => %w(h1 h2 h3 h4 h5 h6 p a img br span table th tr td b ol ul li), :attributes => %w(id class style href src width height)
   end  
 end
