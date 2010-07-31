0eb89a790edba11a28f8c80ba1229d02438766e8
Zheng-Yu Chen <vvdpzz@gmail.com>
2010-07-31 22:13:14 +0800

    添加了标签页面的样式

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
-    @tags = Tag.all
+    @tags = Tag.paginate :page => params[:page]
     respond_to do |format|
       format.html
     end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ActsAsTaggableOn::Tag
+  def count
+    Question.tagged_with(self).count
+  end
 end
